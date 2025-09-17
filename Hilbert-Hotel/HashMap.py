81afa095dbdba8b535b64f62cd71a0041bed4d9f
Nonnnchern <67011509@kmitl.ac.th>
2025-09-18 00:52:47 +0700

    level +1

---
+++ Hilbert-Hotel/HashMap.py
@@ -0,0 +1,19 @@
+class HashMap:
+   def __init__(self, size):
+      self.size = size
+      self.table = [[] for _ in range(self.size)]
+
+   def __str__(self):
+      pass
+
+   def hash_key(self, key):
+      return None
+
+   def insert(self, key, value):
+      return None
+
+   def search(self, key):
+      return None
+
+   def remove(self, key):
+      return None
---
+++ Hilbert-Hotel/Hotel.py
@@ -0,0 +1,45 @@
+from HashMap import HashMap
+
+class Hotel:
+   def __init__(self, size):
+      self.pinned_guest = HashMap(size)
+
+   def calculate_room_cantor(self):
+      pass
+
+   def add_guest():
+      pass
+
+   def add_guest_to_room():
+      pass
+
+   def sort_room():
+      pass
+
+   def empty_room():
+      pass
+
+   def search_room():
+      pass
+
+   def remove_room():
+      pass
+
+   def code_runtime():
+      pass
+
+   def memory_used():
+      pass
+
+   def save_to_file():
+      pass
+
+   print("1. add guest")
+   print("2. add guest to room manually")
+   print("3. print sorted room")
+   print("4. print empty room")
+   print("5. search room")
+   print("6. remove room")
+   print("7. print all runtime of code")
+   print("8. print memory used")
+   print("9. save to file")
